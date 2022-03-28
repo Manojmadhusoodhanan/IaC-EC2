b1dafd5e568c4bdf376b3c7a0a7a2b7e0e3b3c1d
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-03-28 22:58:13 +0530

    fix resource names

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -44,24 +44,24 @@ resource "aws_route_table" "tfprivate_rt" {
 }
 
 resource "aws_route" "tfpublic_route" {
-  route_table_id         = aws_rt.tfpublic_rt.id
+  route_table_id         = aws_route_table.tfpublic_rt.id
   destination_cidr_block = var.dst_cidr
-  gateway_id             = aws_igw.tfigw.id
+  gateway_id             = aws_internet_gateway.tfigw.id
 }
 
 resource "aws_route" "tfprivate_route" {
-  route_table_id         = aws_rt.tfprivate_rt.id
+  route_table_id         = aws_route_table.tfprivate_rt.id
   destination_cidr_block = var.dst_cidr
 }
 
 resource "aws_route_table_association" "tfpublic_assoc" {
   count          = length(var.public_cidr)
   subnet_id      = aws_subnet.tfpublic_subnet.*.id[count.index]
-  route_table_id = aws_rt.tfpublic_rt.id
+  route_table_id = aws_route_table.tfpublic_rt.id
 }
 
 resource "aws_route_table_association" "tfprivate_assoc" {
   count          = length(var.private_cidr)
   subnet_id      = aws_subnet.tfprivate_subnet.*.id[count.index]
-  route_table_id = aws_rt.tfprivate_rt.id
+  route_table_id = aws_route_table.tfprivate_rt.id
 }
