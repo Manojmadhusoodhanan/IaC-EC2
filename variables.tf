523f1cfe0aaf1c34e8e3909d0b82763cc9f524db
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-03-28 22:07:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "private_cidrs" {
   type = list(any)
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
+
+variable "az" {
+  type = string
+  default = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
+}
