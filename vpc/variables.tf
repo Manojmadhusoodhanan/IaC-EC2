7149dc3fd43ac269ef83a14f0b708d51c955743e
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-03-28 23:53:53 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -13,11 +13,6 @@ variable "private_cidr" {
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "az" {
-  type = string
-  default = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
-}
-
 variable "dst_cidr" {
   type = string
   default = "0.0.0.0/0"
