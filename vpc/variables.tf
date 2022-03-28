1543cbee48a1f7c915ffaadac4c256692e908219
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-03-28 22:44:59 +0530

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -3,12 +3,12 @@ variable "cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_cidrs" {
+variable "public_cidr" {
   type = list(any)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_cidrs" {
+variable "private_cidr" {
   type = list(any)
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
