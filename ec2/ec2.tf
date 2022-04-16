e165097948896805b1463e46363c0e24c4da230f
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-04-16 23:11:18 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -25,6 +25,10 @@ resource "aws_security_group" "ssh-sg" {
     from_port   = 0
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"], 
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
