973e2c50cc3804e7bbc24351b2c2d99f3ccc2a0b
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-04-16 14:08:41 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,23 +1,3 @@
-data "aws_ami" "rhel" {
-  most_recent = true
-  owners = ["769417893008"]
-  filter {
-    name = "name"
-    values = ["redHat-*"]
-  }
-  filter {
-    name = "root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name = "architecture"
-    values = ["x86_64"]
-  }
-}
 variable "region" {
   type = string
   default = "ap-south-1"
