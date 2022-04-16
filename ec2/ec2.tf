4498bb2057798dc350567c75af496c069da06e4a
Sony K Sunny <52253656+sony7760@users.noreply.github.com>
2022-04-16 23:17:15 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,13 +22,13 @@ resource "aws_key_pair" "sony_aws" {
 resource "aws_security_group" "ssh-sg" {
   name = "ssh-sg"
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
